b25fff5b096b7f683c4828728c0849d70f6cf873
sn1k <albertoromero@gmail.com>
2016-01-07 10:56:47 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='Tienda libros',
+	version='0.1',
+	description='Lista deseos',
+	url='https://github.com/sn1k/submodulo-alberto/',
+	author='Alberto Romero',
+	author_email='albertoromeroca@gmail.com',
+	license='GNU GPL',
+	packages=['Tienda libros'],
+	install_requires=['django','wheel'],
+	zip_safe=False)
+
