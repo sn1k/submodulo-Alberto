980964b5d5071beed782dfdf613461a6e6899a59
sn1k <albertoromero@gmail.com>
2016-01-07 13:12:54 +0100

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+#Ejecucion remota del docker
+#Instalacion de docker y descarga de imagen
+def getDocker():
+	run('sudo apt-get update')
+	run('sudo apt-get install -y docker.io')
+	run('sudo docker pull sn1k/submodulo-alberto')
+
+#Ejecucion de docker
+def runDocker():
+	run('sudo docker run -p 80:80 -i -t sn1k/submodulo-alberto')
