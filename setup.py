760e5d1f29f1c8375670ddbafd27615b9c2f8c8a
sn1k <albertoromero@gmail.com>
2016-01-07 10:58:39 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='Tienda libros',
+setup(name='librosmasvendidos',
 	version='0.1',
 	description='Lista deseos',
 	url='https://github.com/sn1k/submodulo-alberto/',
 	author='Alberto Romero',
 	author_email='albertoromeroca@gmail.com',
 	license='GNU GPL',
-	packages=['Tienda libros'],
+	packages=['librosmasvendidos'],
 	install_requires=['django','wheel'],
 	zip_safe=False)
 
